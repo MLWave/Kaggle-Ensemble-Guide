c3449cdbefda696f2c66b8af18fd8fd860c22532
Dat Le <thedat.lenguyen@gmail.com>
2015-07-24 11:31:39 +0800

    Tab to 2 spaces.

--- kaggle_avg.py
+++ kaggle_avg.py
@@ -6,19 +6,19 @@ glob_files = sys.argv[1]
 loc_outfile = sys.argv[2]
 
 def kaggle_bag(glob_files, loc_outfile, method="average", weights="uniform"):
-	if method == "average":
-		scores = defaultdict(float)
-	with open(loc_outfile,"wb") as outfile:
-		for i, glob_file in enumerate( glob(glob_files) ):
-			print "parsing:", glob_file
-			for e, line in enumerate( open(glob_file) ):
-				if i == 0 and e == 0:
-					outfile.write(line)
-				if e > 0:
-					row = line.strip().split(",")
-					scores[(e,row[0])] += float(row[1])
-		for j,k in sorted(scores):
-			outfile.write("%s,%f\n"%(k,scores[(j,k)]/(i+1)))
-		print("wrote to %s"%loc_outfile)
+  if method == "average":
+    scores = defaultdict(float)
+  with open(loc_outfile,"wb") as outfile:
+    for i, glob_file in enumerate( glob(glob_files) ):
+      print "parsing:", glob_file
+      for e, line in enumerate( open(glob_file) ):
+        if i == 0 and e == 0:
+          outfile.write(line)
+        if e > 0:
+          row = line.strip().split(",")
+          scores[(e,row[0])] += float(row[1])
+    for j,k in sorted(scores):
+      outfile.write("%s,%f\n"%(k,scores[(j,k)]/(i+1)))
+    print("wrote to %s"%loc_outfile)
 
 kaggle_bag(glob_files, loc_outfile)
